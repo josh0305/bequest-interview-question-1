e32093d9f45bcfb215319d3bd5a2cfe5b575c6f4
Joshua Huang <joshua@Joshuade-MacBook-Pro.local>
2024-01-04 23:20:08 -0500

    implement verify and automatic recovery

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ const API_URL = "http://localhost:8080";
 
 function App() {
   const [data, setData] = useState<string>();
+  const [reservedData, setReservedData] = useState<any>("hhh") // used to save the newest data client send
+  const [verifyRes, setVerifyRes] = useState("It is init status now. If you'd like to verify your data, please press the Verify button")
 
   useEffect(() => {
     getData();
@@ -16,6 +18,14 @@ function App() {
   };
 
   const updateData = async () => {
+    setReservedData(data!)
+
+    await updateWithData(data)
+
+    await getData();
+  };
+
+  const updateWithData =async (data:string | undefined) => {
     await fetch(API_URL, {
       method: "POST",
       body: JSON.stringify({ data }),
@@ -24,12 +34,21 @@ function App() {
         "Content-Type": "application/json",
       },
     });
-
-    await getData();
-  };
+  }
 
   const verifyData = async () => {
-    throw new Error("Not implemented");
+    if(reservedData != null) {
+      await getData();
+      if(data === reservedData){
+        setVerifyRes("The data matched, don't worry")
+      } else {
+        setVerifyRes("Oops, the data doesn't match! The data will be automatically fixed, please verify again!")
+        await updateWithData(reservedData)
+        await getData();
+      }
+    } else {
+      setVerifyRes("Please update the data before verification")
+    }
   };
 
   return (
@@ -62,6 +81,12 @@ function App() {
         <button style={{ fontSize: "20px" }} onClick={verifyData}>
           Verify Data
         </button>
+      </div>
+      <div>
+        <label>{verifyRes}</label>
+      </div>
+      <div>
+        <label>{reservedData}</label>
       </div>
     </div>
   );
